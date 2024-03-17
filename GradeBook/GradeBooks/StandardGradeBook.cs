7a23d0328607bbfd5176552ff362e39fea9bfda8
Michał Białas <michal.bialas@microsoft.wsei.edu.pl>
2024-03-17 15:23:15 +0100

    Dodanie klasy dziedziczonej i konstuktora

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+namespace GradeBook.GradeBooks
+{
+    internal class StandardGradeBook:BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            type = Enums.GradeBookType.Standard;
+        }
+    }
+}
