540f600f5b63b4f6016c4f77f95b3edb97e4f211
Michał Białas <michal.bialas@microsoft.wsei.edu.pl>
2024-03-17 15:33:59 +0100

    dodata metoda override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,18 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name):base(name)
         {
             type = Enums.GradeBookType.Ranked;
+        }
+        public override void CalculateStatistics()
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+            
         }
     }
 }
